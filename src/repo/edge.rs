d03de2c229ae0e23df2fb05e3fb88608a6c63f13
Nathan Faucett <nathanfaucett@gmail.com>
2024-12-15 11:31:00 -0500

    remove macro queries

--- src/repo/edge.rs
+++ src/repo/edge.rs
@@ -9,42 +9,6 @@ pub struct EdgeRow {
   pub created_at: i64,
 }
 
-pub async fn get_edge_by_id(pool: &sqlx::SqlitePool, id: i64) -> sqlx::Result<Option<EdgeRow>> {
-  sqlx::query_as!(
-    EdgeRow,
-    "SELECT edge.* FROM edges edge WHERE edge.id = $1;",
-    id
-  )
-  .fetch_optional(pool)
-  .await
-}
-
-pub async fn get_edges_by_from_node_id(
-  pool: &sqlx::SqlitePool,
-  from_node_id: i64,
-) -> sqlx::Result<Vec<EdgeRow>> {
-  sqlx::query_as!(
-    EdgeRow,
-    "SELECT edge.* FROM edges edge WHERE edge.from_node_id = $1;",
-    from_node_id
-  )
-  .fetch_all(pool)
-  .await
-}
-
-pub async fn get_edges_by_to_node_id(
-  pool: &sqlx::SqlitePool,
-  to_node_id: i64,
-) -> sqlx::Result<Vec<EdgeRow>> {
-  sqlx::query_as!(
-    EdgeRow,
-    "SELECT edge.* FROM edges edge WHERE edge.to_node_id = $1;",
-    to_node_id
-  )
-  .fetch_all(pool)
-  .await
-}
-
 pub async fn create_edge(
   pool: &sqlx::SqlitePool,
   from_node_id: i64,
@@ -52,34 +16,27 @@ pub async fn create_edge(
   uri: &str,
   data: Option<&str>,
 ) -> sqlx::Result<EdgeRow> {
-  sqlx::query_as!(
-    EdgeRow,
+  sqlx::query_as(
     "INSERT INTO edges (from_node_id, to_node_id, uri, data) VALUES ($1, $2, $3, $4) RETURNING *;",
-    from_node_id,
-    to_node_id,
-    uri,
-    data
   )
+  .bind(from_node_id)
+  .bind(to_node_id)
+  .bind(uri)
+  .bind(data)
   .fetch_one(pool)
   .await
 }
 
 pub async fn delete_edge(pool: &sqlx::SqlitePool, edge_id: i64) -> sqlx::Result<Option<EdgeRow>> {
-  sqlx::query_as!(
-    EdgeRow,
-    "DELETE FROM edges WHERE id = $1 RETURNING *;",
-    edge_id
-  )
-  .fetch_optional(pool)
-  .await
+  sqlx::query_as("DELETE FROM edges WHERE id = $1 RETURNING *;")
+    .bind(edge_id)
+    .fetch_optional(pool)
+    .await
 }
 
 pub async fn delete_edges_by_uri(pool: &sqlx::SqlitePool, uri: &str) -> sqlx::Result<Vec<EdgeRow>> {
-  sqlx::query_as!(
-    EdgeRow,
-    "DELETE FROM edges WHERE uri = $1 RETURNING *;",
-    uri
-  )
-  .fetch_all(pool)
-  .await
+  sqlx::query_as("DELETE FROM edges WHERE uri = $1 RETURNING *;")
+    .bind(uri)
+    .fetch_all(pool)
+    .await
 }
--- src/repo/node.rs
+++ src/repo/node.rs
@@ -7,49 +7,24 @@ pub struct NodeRow {
   pub created_at: i64,
 }
 
-pub async fn get_node_by_id(pool: &sqlx::SqlitePool, id: i64) -> sqlx::Result<Option<NodeRow>> {
-  sqlx::query_as!(
-    NodeRow,
-    "SELECT node.* FROM nodes node WHERE node.id = $1;",
-    id
-  )
-  .fetch_optional(pool)
-  .await
-}
-
-pub async fn get_nodes_by_uri(pool: &sqlx::SqlitePool, uri: &str) -> sqlx::Result<Vec<NodeRow>> {
-  sqlx::query_as!(
-    NodeRow,
-    "SELECT node.* FROM nodes node WHERE node.uri = $1;",
-    uri
-  )
-  .fetch_all(pool)
-  .await
-}
-
 pub async fn create_node(pool: &sqlx::SqlitePool, uri: &str, data: &str) -> sqlx::Result<NodeRow> {
-  sqlx::query_as!(
-    NodeRow,
-    "INSERT INTO nodes (uri, data) VALUES ($1, $2) RETURNING *;",
-    uri,
-    data
-  )
-  .fetch_one(pool)
-  .await
+  sqlx::query_as("INSERT INTO nodes (uri, data) VALUES ($1, $2) RETURNING *;")
+    .bind(uri)
+    .bind(data)
+    .fetch_one(pool)
+    .await
 }
 
 pub async fn delete_node(pool: &sqlx::SqlitePool, id: i64) -> sqlx::Result<Option<NodeRow>> {
-  sqlx::query_as!(NodeRow, "DELETE FROM nodes WHERE id = $1 RETURNING *;", id)
+  sqlx::query_as("DELETE FROM nodes WHERE id = $1 RETURNING *;")
+    .bind(id)
     .fetch_optional(pool)
     .await
 }
 
 pub async fn delete_nodes_by_uri(pool: &sqlx::SqlitePool, uri: &str) -> sqlx::Result<Vec<NodeRow>> {
-  sqlx::query_as!(
-    NodeRow,
-    "DELETE FROM nodes WHERE uri = $1 RETURNING *;",
-    uri
-  )
-  .fetch_all(pool)
-  .await
+  sqlx::query_as("DELETE FROM nodes WHERE uri = $1 RETURNING *;")
+    .bind(uri)
+    .fetch_all(pool)
+    .await
 }
